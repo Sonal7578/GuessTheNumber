4071b83d8ca6b4c2af2670cc777f3a5609ad0cb4
Sonal <sonal.dhage26@gmail.com>
2024-09-22 21:19:34 +0200

    implemented the guess function completely

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,3 +1,14 @@
 import random
 
-def guess(x) :
+def guess(x):
+    random_num = random.randint(1, x)
+    guess = 0
+    while guess != random_num:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_num:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_num:
+            print("Sorry, guess again. Too high.")
+    print(f"Congratulations! You've guessed the number {random_num}!")
+
+guess(10)
